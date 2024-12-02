52af6cfc9ca2c5887e819c10cf2916d49b75b99b
ynhind <118122418+ynhind@users.noreply.github.com>
2024-12-03 00:26:44 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import { Routes, Route } from "react-router-dom";
-import Page1 from "./pages/Page1/Page1.jsx"
 import TableList from "./pages/Patient/TableList.jsx";
 import MedicalRecord from "./pages/Patient/Medical Record/MedicalRecord.jsx";
 import HomePage from "./pages/Home/HomePage.jsx";
 import Information from "./pages/Patient/Medical Record/Information.jsx"
+import AppointmentPage from "./pages/Appointment/AppointmentPage.jsx";
+
 import DoctorProfile from "./pages/Doctor/Profile.jsx"
+
 import TopLoader from "./components/TopLoader.jsx";
-import Footer from "./components/Footer.jsx"
 
 
 const App = () => {
     return (
         <div>
-            <div className="relative">
+            <div className="relative overflow-hidden">
                 <div
                     className="bg-[#4cbcd831] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]"
                 />
                 <div
                     className="bg-[#6285c25b] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
                 />
-                <div className="flex flex-col min-h-screen">
+                <div className="flex flex-col h-screen">
                     <TopLoader />
-                    <div className="flex-grow pb-20">
                     <Routes>
                         <Route
                             path="/*"
@@ -31,7 +31,7 @@ const App = () => {
                                 </div>
                             }
                         />
-                        <Route path="/page1/*" element={<Page1 />} />
+                        <Route path="Appointment/*" element={<AppointmentPage />} />
                         <Route path="/tableList" element={<TableList />} />
                         <Route path="/medicalRecord" element={<MedicalRecord />} />
                         <Route path="/information" element={<Information />} />
@@ -39,9 +39,6 @@ const App = () => {
 
                     </Routes>
                 </div>
-            </div>
-
-            <Footer />
             </div>
         </div>
     );

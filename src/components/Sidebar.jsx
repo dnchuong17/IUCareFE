ece9a45fb71a81662102ac950280db4fdc08bed4
ynhind <118122418+ynhind@users.noreply.github.com>
2024-12-09 18:15:58 +0700

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
         <img
           src={assets.sidebar_logo}
           alt="Sidebar Logo"
-          className="w-full h-auto mb-4 md:p-1 md:top-0 hover:scale-105 border border-gray-300 rounded transition-transform duration-300 top-4 left-4"
+          className="w-full h-auto mb-4 md:p-1 md:top-0 hover:scale-105 rounded transition-transform duration-300 top-4 left-4"
         />
         <div className="flex flex-col space-y-5 mb-12 ">
           <div className="group flexCenter space-x-1 mt-4 bg-white-500 text-gray-600 p-2 md:p-1 rounded-lg mt-5 cursor-pointer px-3 hover:bg-blue-200 hover:bg-opacity-50 hover:text-blue-700 transition-colors duration-300">
